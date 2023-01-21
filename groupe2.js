24303ba58325e30025ef74eb69ce2ef75ca0e453
Cyril Hamel <xizor64@gmail.com>
2023-01-21 18:31:46 +0100

    majGroupe2

--- groupe2.js
+++ groupe2.js
@@ -3,7 +3,7 @@ let divWin = document.getElementById("divWin");
 let nbDePasTotal = document.getElementById("nbDePasTotal");
 let affichage = document.getElementById('compte');
 let tabTest = [0,0];
-let result = document.getElementById("#resultat");
+let result = document.getElementById("resultat");
 
 function déplacement() {
     compteur++;
@@ -15,9 +15,12 @@ function afficher() {
 }
 
 function win(){
+    let coupMin=tabTest.length-1;
     nbDePasTotal.textContent = "nombre de pas :" + " "+ compteur;
-    pasMax.textContent = "le nombre minimal de pas était de : "+ tabTest.length;
-    result.innerText = `Vous avez joué ${compteur} coup, vous auriez pu le faire en ${tabTest.length} coups vous pouvez réessayer et le faire en ${compteur - tabTest.length}`;
+    pasMax.textContent = "le nombre minimal de pas était de : "+ coupMin;
+if ((coupMin - compteur) > 0){
+    result.innerText = `Vous avez joué ${compteur} coup, vous auriez pu le faire en ${coupMin} coups vous pouvez réessayer et le faire en ${compteur - coupMin}`;
+} 
     divWin.classList.add("pasCacher");
 }
 
